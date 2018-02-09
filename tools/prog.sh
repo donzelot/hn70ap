72a47d63b6c642df5184aabb4f061b8af061f4cf
Sebastien F4GRX <f4grx@f4grx.net>
2018-02-09 03:27:23 +0100

    update defconfig with debug, add ocd tools

---
+++ tools/prog.sh
@@ -0,0 +1,7 @@
+openocd \
+	-f interface/ftdi/jtag-lock-pick_tiny_2.cfg \
+	-c "transport select jtag" \
+	-c "reset_config srst_only" \
+	-f target/stm32f4x.cfg \
+	-c "program nuttx verify reset exit"
+
