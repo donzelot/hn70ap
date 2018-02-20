6852c2bb45e4c27b6b829922f1bf7c94acb50b00
Sebastien F4GRX <f4grx@f4grx.net>
2018-02-20 12:38:03 +0100

    fix indentation

--- src/bootloader_gpio.c
+++ src/bootloader_gpio.c
@@ -23,61 +23,65 @@
 
 BOOTCODE void bootloader_gpio_init(uint32_t gpiodesc)
 {
-	uint32_t line = (gpiodesc & GPIO_FLAGS_MASK_LINE) >> GPIO_FLAGS_SHIFT_LINE;
-	uint32_t port = (gpiodesc & GPIO_FLAGS_MASK_PORT) >> GPIO_FLAGS_SHIFT_PORT;
-	uint32_t base, val, dir;
+  uint32_t line = (gpiodesc & GPIO_FLAGS_MASK_LINE) >> GPIO_FLAGS_SHIFT_LINE;
+  uint32_t port = (gpiodesc & GPIO_FLAGS_MASK_PORT) >> GPIO_FLAGS_SHIFT_PORT;
+  uint32_t base, val;
 
-	//get port base address
-	if(port >= 10) return;
-	base = GPIO_BASE + (port * GPIO_STEP);
+  //get port base address
+  if(port >= 10)
+    {
+      return;
+    }
+  base = GPIO_BASE + (port * GPIO_STEP);
 
-	//Enable clock to GPIO peripheral
-    modreg32(RCC_AHB1ENR, 1<<port, 0);
+  //Enable clock to GPIO peripheral
+  modreg32(RCC_AHB1ENR, 1<<port, 0);
 
-    dir = (gpiodesc & GPIO_FLAGS_MASK_MODE) >> GPIO_FLAGS_SHIFT_MODE;
 
-    //configure 1-bit ports
-    if(dir == 1) { //Output only
-        //Initial state
-        val = (gpiodesc & GPIO_FLAGS_MASK_INIT) >> GPIO_FLAGS_SHIFT_INIT;
-        bootloader_gpio_write(gpiodesc, val);
+  //configure 1-bit ports
+  //Define initial state Initial state and output type (output only)
+  if((gpiodesc & GPIO_FLAGS_MASK_MODE) == GPIO_MODE_OUT)
+    {
+      val = (gpiodesc & GPIO_FLAGS_MASK_INIT) >> GPIO_FLAGS_SHIFT_INIT;
+      bootloader_gpio_write(gpiodesc, val);
 
-        //Offset 04 OTYPER
-        val = (gpiodesc & GPIO_FLAGS_MASK_TYPE) >> GPIO_FLAGS_SHIFT_TYPE;
-        modreg32(base + GPIO_REGOFF_OTYPER, val << line, 1 << line);
+      val = (gpiodesc & GPIO_FLAGS_MASK_TYPE) >> GPIO_FLAGS_SHIFT_TYPE;
+      modreg32(base + GPIO_REGOFF_OTYPER, val << line, 1 << line);
     }
 	
-	//configure 2-bit ports
-	line += line;
+  //configure 2-bit ports
+  line += line;
 	
-	//Offset 00 MODER
-	val = dir;
-    modreg32(base + GPIO_REGOFF_MODER, val << line, 3 << line);
+  //Define pin mode
+  val = (gpiodesc & GPIO_FLAGS_MASK_MODE) >> GPIO_FLAGS_SHIFT_MODE;
+  modreg32(base + GPIO_REGOFF_MODER, val << line, 3 << line);
 	
-    if(dir == 1) { //Output only
-        //Offset 08 OSPEEDER
-        val = (gpiodesc & GPIO_FLAGS_MASK_SPD) >> GPIO_FLAGS_SHIFT_SPD;
-        modreg32(base + GPIO_REGOFF_OSPEEDER, val << line, 3 << line);
+  //Define output speed (output only)
+  if((gpiodesc & GPIO_FLAGS_MASK_MODE) == GPIO_MODE_OUT)
+    {
+      val = (gpiodesc & GPIO_FLAGS_MASK_SPD) >> GPIO_FLAGS_SHIFT_SPD;
+      modreg32(base + GPIO_REGOFF_OSPEEDER, val << line, 3 << line);
     }
 
-    if(dir == 0) { //Input only
-        //Offset 0C PUPDR
-        val = (gpiodesc & GPIO_FLAGS_MASK_PULL) >> GPIO_FLAGS_SHIFT_PULL;
-        modreg32(base + GPIO_REGOFF_PUPDR, val << line, 3 << line);
-    }
+  //Define pull mode
+  val = (gpiodesc & GPIO_FLAGS_MASK_PULL) >> GPIO_FLAGS_SHIFT_PULL;
+  modreg32(base + GPIO_REGOFF_PUPDR, val << line, 3 << line);
 
-	//configure 4-bit ports
-	line += line;
+  //configure 4-bit ports
+  line += line;
 	
-	//Offset 20 AFRL (for lines 0-7)
-	//Offset 24 AFRH (for lines 8-15)
-	val = (gpiodesc & GPIO_FLAGS_MASK_ALT) >> GPIO_FLAGS_SHIFT_ALT;
-	if(line < (8 << 2)) {
-        modreg32(base + GPIO_REGOFF_AFRL, val << line, 15 << line);
-	} else {
-        line -= 32;
-        modreg32(base + GPIO_REGOFF_AFRL, val << line, 15 << line);
-	}
+  //Offset 20 AFRL (for lines 0-7)
+  //Offset 24 AFRH (for lines 8-15)
+  val = (gpiodesc & GPIO_FLAGS_MASK_ALT) >> GPIO_FLAGS_SHIFT_ALT;
+  if(line < (8 << 2))
+    {
+      modreg32(base + GPIO_REGOFF_AFRL, val << line, 15 << line);
+    }
+  else
+    {
+      line -= 32;
+      modreg32(base + GPIO_REGOFF_AFRL, val << line, 15 << line);
+    }
 }
 
 BOOTCODE void bootloader_gpio_write(uint32_t gpio, int state)
@@ -87,10 +91,16 @@ BOOTCODE void bootloader_gpio_write(uint32_t gpio, int state)
   uint32_t base;
 
   //get port base address
-  if(port >= 10) return;
+  if(port >= 10)
+    {
+      return;
+    }
   base = GPIO_BASE + (port * GPIO_STEP);
 
-  if(!state) line += 16; //access BR instead of BS
+  if(!state)
+    {
+      line += 16; //access BR instead of BS
+    }
   base += GPIO_REGOFF_BSRR;
   putreg32(base, 1<<line);
 }
@@ -102,7 +112,10 @@ BOOTCODE int bootloader_gpio_read(uint32_t gpio)
   uint32_t base;
 
   //get port base address
-  if(port >= 10) return;
+  if(port >= 10)
+    {
+      return 0;
+    }
   base = GPIO_BASE + (port * GPIO_STEP);
 
   return (getreg32(base + GPIO_REGOFF_IDR) >> line) & 1;
